dcee04ffd32652943003f4ef65384f6ee50d6eee
Vince Nieuwenburg <vince@MacBookAir.home>
2025-02-10 15:36:20 +0100

    Working example of deployed EC2 instance using Terraform.

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-west-1"
-  profile = "vince-euwest1"
+  region  = "eu-north-1"
+  profile = "vince-1"
 }
 
 resource "aws_instance" "hello_world" {
-  ami           = "ami-0298982da8499a634"
-  instance_type = "t2.micro"
+  ami           = "ami-08b1d20c6a69a7100"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "TechChallenge"
